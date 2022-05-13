fd1aa9006887af6438b4b87cdd64c30429556f79
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-05-13 09:47:59 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Home/Shared/Navbar';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
