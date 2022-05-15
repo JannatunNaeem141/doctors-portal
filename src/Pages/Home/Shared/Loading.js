0c706097a0fdfd3eb52ffb706d53219cd14838b4
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-05-15 10:44:33 +0600

    Login form validation done

---
+++ src/Pages/Home/Shared/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='h-screen flex justify-center items-center'>
+            <button class="btn loading">loading</button>
+        </div>
+    );
+};
+
+export default Loading;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import Loading from '../Home/Shared/Loading';
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
 
-    if (user) {
-        console.log(user);
+    let signInError;
+
+    if (loading || gLoading) {
+        return <Loading></Loading>
+    }
+
+    if (error || gError) {
+        signInError = <small className='text-red-500'>{error?.message || gError?.message}</small>
+    }
+
+    if (gUser) {
+        console.log(gUser);
     }
     const onSubmit = data => {
         console.log(data);
+        signInWithEmailAndPassword(data.email, data.password);
     }
     return (
         <div className='flex h-screen justify-center items-center'>
@@ -65,6 +83,7 @@ const Login = () => {
                             </label>
                         </div>
 
+                        {signInError}
                         <input className='btn w-full max-w-xs ' type="submit" value='Login' />
                     </form>
                     <div className="divider">OR</div>
