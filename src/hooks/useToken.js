1c69f9aae566bcbe3f5d66a0a799efe083351409
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-05-16 20:35:34 +0600

    JWT token created and applied

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -15,7 +15,10 @@ const useToken = user => {
             })
                 .then(res => res.json())
                 .then(data => {
-                    console.log('data inside useToken', data)
+                    console.log('data inside useToken', data);
+                    const accessToken = data.token;
+                    localStorage.setItem('accessToken', accessToken);
+                    setToken(accessToken);
                 })
         }
     }, [user]);
