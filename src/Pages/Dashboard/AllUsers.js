da75fc98f19fbafda21899e41c3ab6dae2a64b92
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-05-16 23:34:17 +0600

    Loaded all user in the admin page & created api to make admin

--- src/Pages/Dashboard/AllUsers.js
+++ src/Pages/Dashboard/AllUsers.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Home/Shared/Loading';
+import UserRow from './UserRow';
 
 const AllUsers = () => {
+
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h2>All Users</h2>
+            <h2>All Users: {users.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Index</th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => <UserRow
+                                key={user._id}
+                                user={user}
+                                refetch={refetch}
+                            ></UserRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Dashboard/UserRow.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user;
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accesstoken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                refetch();
+                toast.success('Successfully make an admin');
+            })
+    }
+    return (
+        <tr>
+            <th>1</th>
+            <td>{email}</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
+            <td><button class="btn btn-xs">Remove User</button></td>
+        </tr>
+    );
+};
+
+export default UserRow;
